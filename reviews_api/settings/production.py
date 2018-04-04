d2de4d25ddb2df00b5c92ff753fa0e9e9567a9dd
Nayara Caetano <nayara.caetanopinheiro@gmail.com>
2018-04-04 09:29:49 -0300

    Auth in production only with JsonWebToken

--- reviews_api/settings/production.py
+++ reviews_api/settings/production.py
@@ -8,3 +8,7 @@ REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
     'rest_framework.renderers.JSONRenderer',
     'rest_framework.renderers.MultiPartRenderer',
 )
+
+REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = (
+    'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+)
