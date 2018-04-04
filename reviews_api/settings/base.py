96bf40cdef4ae8c0a3dff0a302b4d8c41f1188f2
Nayara Caetano <nayara.caetanopinheiro@gmail.com>
2018-04-04 18:22:58 -0300

    Add heroku deploy configs

--- reviews_api/settings/base.py
+++ reviews_api/settings/base.py
@@ -113,6 +113,7 @@ AUTH_USER_MODEL = 'authentication.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
--- reviews_api/settings/production.py
+++ reviews_api/settings/production.py
@@ -1,7 +1,14 @@
 from .base import *
 
+import dj_database_url
+
 
 DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Remove browsable API enabled in base
 REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
